169e652c55d88c735af147e6accc2ce961ed15ba
Sandra Madarnas <sandra.madarnas.1@gmail.com>
2023-07-01 21:34:09 +0000

    Actualizando cambios

--- src/App.js
+++ src/App.js
@@ -1,31 +1,5 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
 import React, { useState, useEffect } from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -49,20 +23,42 @@ function App() {
   }, []);
 
   return (
-    <div className="app">
-      <h1>Tarjetas</h1>
-      {cards.map((card, index) => (
-        <div className="card" key={index}>
-          <img src={card.photo} alt="Foto" />
-          <h2>{card.name} {card.lastName}</h2>
-          <p>DNI: {card.dni}</p>
-          <p>Estado Civil: {card.maritalStatus}</p>
-          <p>Código Postal: {card.postalCode}</p>
-        </div>
-      ))}
-    </div>
+    <>
+      <div className="app">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h1>Tarjetas</h1>
+        {cards.map((card, index) => (
+          <div className="card" key={index}>
+            <img src={card.photo} alt="Foto" />
+            <h2>{card.name} {card.lastName}</h2>
+            <p>DNI: {card.dni}</p>
+            <p>Estado Civil: {card.maritalStatus}</p>
+            <p>Código Postal: {card.postalCode}</p>
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+              Launch demo modal
+            </button>
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+              <div class="modal-dialog">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  </div>
+                  <div class="modal-body">
+                    ...
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
 export default App;
-
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
+// import ReactDOM from "react-dom";
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import NavBar from './components/NavBar.jsx';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <NavBar />
     <App />
   </React.StrictMode>
 );
